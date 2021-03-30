2709fa9c26e3f39ddca832a4887cbce0473bc0fa
root <root@localhost.localdomain>
2021-03-30 02:07:15 +0100

    Implement php script for login check

--- App/connect.php
+++ App/connect.php
@@ -1,13 +1,25 @@
 <?php
 
-session_start();
+if(!isset($_SESSION)){session_start();}
 
-function hasSessionStart(){
-	return isset($_SESSION['init']) ? true : false;
+try{
+	$local = "localhost";
+	$dbname = "yourMarketDB";
+	$user = "www-data";
+	$pass = "toor";
+	$db = new PDO("mysql:host=$local;dbname=$dbname;charset=utf8",$user,$pass);
+
+}catch(Exeption $e){
+				die("ERROR".$e->intl_get_error_message());
 }
-function startSession(){
-	if(hasSessionStart()){
 
+if(isset($_POST['inputEmail']) && isset($_POST['inputPassword'])){
+	$userEmail = (String)$_POST['inputEmail'];
+	$userPasswd = (String)$_POST['inputPassword'];
+	// Create object to verify connection
+	$user_connect_attempt = new db_user($db,$userEmail);
+	if($user_connect_attempt->hasPasswd($userPasswd)){
+		$_SESSION["db_user"]=$user_connect_attempt;
 	}
 }
 
